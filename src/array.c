6c374320270c115e5d785cf212daab449588332c
aikawa hiroki <suuya78@aikawanoMacBook-ea.local>
2019-06-25 11:43:24 +0900

    comp

--- src/array.c
+++ src/array.c
@@ -4,15 +4,31 @@ int n;
 int k;
 int A[100000];
 
+int p(int x){
+  int sum = 0;
+  int i;
+  for(i = 0; i < n; i++){
+    sum += (A[i]+x-1)/x;
+  }
+  return sum <= k;
+}
 
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
+  int sum = 0;
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb = 0;
+  ub = 100000;
+  while(ub - lb > 1){
+    int m = (lb + ub) / 2;
+    if(p(m)) ub = m;
+    else lb = m;
+  }
+  
+  printf("%d\n",ub);
 
   return 0;
 }
