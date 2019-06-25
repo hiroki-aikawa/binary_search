2494dc94b246906d01103d690af20a40c682b5d5
aikawa hiroki <suuya78@aikawanoMacBook-ea.local>
2019-06-25 11:43:09 +0900

    comp

--- src/apple.c
+++ src/apple.c
@@ -10,8 +10,14 @@ int main(){
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
+    }
+  lb = -1;
+  ub = n;
+  while(ub - lb > 1){
+    int m = (lb + ub)/2;
+    if(A[m] >= k) ub =m;
+    else lb = m;
   }
-
-
+  printf("%d\n", ub);
   return 0;
 }
