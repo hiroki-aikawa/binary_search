faf0113262779c6916c5407fffe4cd496ca41fa8
aikawa hiroki <suuya78@aikawanoMacBook-ea.local>
2019-06-25 11:43:47 +0900

    comp

--- src/works.c
+++ src/works.c
@@ -4,6 +4,23 @@ int n;
 int k;
 int A[100000];
 
+int p(int x){
+  int a = x;
+  int b = 1;
+  int i;
+  for(i = 0; i < n; i++){
+    if(x < A[i]){
+      b = n + 1;
+      break;
+    }
+    else if(a >= A[i]) a -= A[i];
+    else{
+      a = x - A[i];
+      b++;
+    }
+  }
+  return b <= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +28,15 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
+  lb = 0;
+  ub = 100000;
+  while(ub - lb > 1){
+    int m = (lb + ub) / 2;
+    if(p(m)) ub = m;
+    else lb = m;
+  }
 
+  printf("%d\n",ub);
 
   return 0;
 }
